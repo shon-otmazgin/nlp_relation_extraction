878bfdaa75cb980504b71aecebe0433defcca4b9
shon-otmazgin <shon711@gmail.com>
2021-01-08 14:47:31 +0200

    build train dataframe for annotaion

--- relation_extraction.py
+++ relation_extraction.py
@@ -1,48 +1,115 @@
-from utils import CORPUS, ANNOTATIONS, PROCESSED_CORPUS, LIVE_IN, WORK_FOR, FIELDS_H
+import pandas as pd
+import itertools
+
+from spacy.tokens.span import defaultdict
+
+from utils import CORPUS, ANNOTATIONS, PROCESSED_CORPUS, LIVE_IN, WORK_FOR, FIELDS_H, ENTTYPE, FORM, ENTIBO, HEAD, \
+    LEMMA, ID, DEPREL
+import sys
+import numpy as np
+import spacy
+import codecs
+from tqdm import tqdm
+
+pd.set_option("display.max_rows", None, "display.max_columns", None, 'display.width', None)
 
 
-def read_corpus(file):
-    with open(file, 'r') as f:
-        return dict(line.split('\t') for line in f.read().splitlines())
+def pairwise(iterable):
+    "s -> (s0, s1), (s2, s3), (s4, s5), ..."
+    a = iter(iterable)
+    return zip(a, a)
 
 
-def read_annotation(file):
-    with open(file, 'r') as f:
-        return dict((line.split('\t')[0], tuple(line.split('\t')[1:-1])) for line in f.read().splitlines())
+def extract_features(ent1, ent2, sent, sent_id):
+
+    # Entity-based features
+    embedding = ent1.root.vector
+    embedding += ent2.root.vector
+
+    # Word-based features
+    embedding += nlp(sent.text[ent1.end_char:ent2.end_char-len(ent2.text)]).vector
+    embedding += nlp(sent.text[:ent1.end_char-len(ent1.text)].split()[-1]).vector if len(sent.text[:ent1.end_char-len(ent1.text)]) > 0 else 0
+    embedding += nlp(sent.text[ent2.end_char:].split()[0]).vector if len(sent.text[ent2.end_char:]) > 0 else 0
+
+    # Syntactic features
+    # features['constituent_path'] = None
+    # features['basic_syntactic_chunk_path'] = None
+    # features['typed_dependency_path'] = dependency_path(ent1, ent2)
+
+    features = pd.Series(embedding, name=(sent_id, ent1.text, ent2.text))
+    features['ent1_type'] = ent1.root.ent_type_
+    features['ent2_type'] = ent2.root.ent_type_
+    features['concat_type'] = features['ent1_type'] + features['ent2_type']
+    features['y'] = 0
+    return features
 
 
-def read_processed_corpus(file):
-    def tokenize(tokens):
-        if len(tokens) == 1:
-            return tokens[0]
-        return {h: v for h, v in zip(FIELDS_H, tokens)}
+def dependency_path(ent1, ent2):
+    dep_path = []
 
-    processed_corpus = {}
-    with open(file, 'r', encoding='utf8') as f:
-        sentence = []
-        for line in f:
-            line = line.rstrip('\n')
-            if line:
-                sentence.append(tokenize(line.split('\t')))
-            else:
-                processed_corpus[sentence[0].split(':')[1].strip()] = sentence[2:]
-                sentence = []
-    return processed_corpus
+    tok = ent1.root
+    while tok.dep_ != 'ROOT':
+        dep_path.append((tok.text, tok.dep_))
+        tok = tok.head
+
+    i = len(dep_path)
+
+    tok = ent2.root
+    while True:
+        dep_path.insert(i, (tok.text, tok.dep_))
+        if tok.dep_ == 'ROOT':
+            break
+        tok = tok.head
+
+    return dep_path
 
 
-train_set = {CORPUS: read_corpus(file='data\Corpus.TRAIN.txt'),
-             ANNOTATIONS: read_annotation(file='data\TRAIN.annotations'),
-             PROCESSED_CORPUS: read_processed_corpus(file='data\Corpus.TRAIN.processed')}
+def read_lines(fname):
+    sentences = []
+    for line in codecs.open(fname, encoding="utf8"):
+        sent_id, sent = line.strip().split("\t")
+        sent = sent.replace("-LRB-","(")
+        sent = sent.replace("-RRB-",")")
+        sentences.append((sent_id, sent))
+    return sentences
 
-dev_set = {CORPUS: read_corpus(file='data\Corpus.DEV.txt'),
-           ANNOTATIONS: read_annotation(file='data\DEV.annotations'),
-           PROCESSED_CORPUS: read_processed_corpus(file='data\Corpus.DEV.processed')}
 
-# live = 0
-# work = 0
-# for sent_id, annot in train_set[ANNOTATIONS].items():
-#     if annot[1] == LIVE_IN: live += 1
-#     elif annot[1] == WORK_FOR: work += 1
-#
-# print(live)
-# print(work)
+def read_annotations(fname):
+    annotations = defaultdict(lambda: [])
+    for line in codecs.open(fname, encoding="utf8"):
+        sent_id, arg1, rel, arg2 = line.strip().split("\t")[0:4]
+        annotations[sent_id].append((arg1, rel, arg2))
+    return annotations
+
+
+nlp = spacy.load('en_core_web_lg')
+
+ENTITIES_TYPE = ['PERSON', 'ORG']
+
+train_annotations = read_annotations(sys.argv[2])
+
+c = 0
+for sent_id, annotations in train_annotations.items():
+    for ann in annotations:
+        if ann[1] == WORK_FOR:
+            # print(f'{sent_id} {ann}')
+            c+=1
+print(f'train {WORK_FOR} annotations: {c}')
+
+train_df = pd.DataFrame()
+for sent_id, sent_str in tqdm(read_lines(sys.argv[1])):
+    sent = nlp(sent_str)
+    for ent1, ent2 in itertools.combinations(sent.ents, 2):
+        if ent1.root.ent_type_ not in ENTITIES_TYPE:
+            continue
+        if ent2.root.ent_type_ not in ENTITIES_TYPE:
+            continue
+        if ent1.root.ent_type_ == ent2.root.ent_type_:
+            continue
+        features = extract_features(ent1, ent2, sent, sent_id)
+        train_df = train_df.append(features)
+
+train_df = pd.get_dummies(train_df, columns=['concat_type', 'ent1_type', 'ent2_type'])
+
+print(train_df.shape)
+
--- spc.py
+++ spc.py
@@ -2,8 +2,7 @@ import codecs
 import spacy
 import sys
 
-
-nlp = spacy.load('en')
+nlp = spacy.load('en_core_web_sm')
 
 def read_lines(fname):
     for line in codecs.open(fname, encoding="utf8"):
@@ -13,20 +12,22 @@ def read_lines(fname):
         yield sent_id, sent
 
 for sent_id, sent_str in read_lines(sys.argv[1]):
+    if sent_id != 'sent52':
+        continue
     sent = nlp(sent_str)
-    print "#id:",sent_id
-    print "#text:",sent.text
+    print(f"#id: {sent_id}")
+    print(f"#text: {sent.text}")
     for word in sent:
         head_id = str(word.head.i+1)        # we want ids to be 1 based
         if word == word.head:               # and the ROOT to be 0.
             assert(word.dep_=="ROOT"),word.dep_
             head_id = "0" # root
-        print "\t".join([str(word.i+1), word.text, word.lemma_, word.tag_, word.pos_, head_id, word.dep_, word.ent_iob_, word.ent_type_])
-    print
-    # print "#", Noun Chunks:
-    # for np in sent.noun_chunks:
-    #    print(np.text, np.root.text, np.root.dep_, np.root.head.text)
-    # print "#", named entities:
-    # for ne in sent.ents:
-    #    print(ne.text, ne.root.ent_type_, ne.root.text, ne.root.dep_, ne.root.head.text)
+        print("\t".join([str(word.i+1), word.text, word.lemma_, word.tag_, word.pos_, head_id, word.dep_, word.ent_iob_, word.ent_type_]))
 
+    print("#", 'Noun Chunks:')
+    for np in sent.noun_chunks:
+        print(np.text, np.root.text, np.root.dep_, np.root.head.text)
+    print("#", 'named entities:')
+    for ne in sent.ents:
+        print(ne.text, ne.root.ent_type_, ne.root.text, ne.root.dep_, ne.root.head.text)
+    print()
