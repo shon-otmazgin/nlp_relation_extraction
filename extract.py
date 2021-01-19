bdc3b74a2766809d1f878d28bade4c4c49bdc1dd
shon-otmazgin <shon711@gmail.com>
2021-01-19 19:04:11 +0200

    update extract.py input and output files

--- extract.py
+++ extract.py
@@ -3,7 +3,7 @@ import pickle
 
 from features_extraction import build_df
 
-with open('train', 'rb') as f:
+with open('trained_model', 'rb') as f:
     lr_clf, V, vocab = pickle.load(f)
 
 test_df, V, vocab = build_df(file=sys.argv[1], V=V, vocab=vocab)
@@ -11,9 +11,10 @@ print(f'Test size: {test_df.shape}')
 
 test_df['y_pred'] = lr_clf.predict(test_df)
 
-with open('predicted_relation.txt', 'w', encoding="utf8") as f:
+output_file = sys.argv[2]
+with open(output_file, 'w', encoding="utf8") as f:
     for idx in test_df[test_df['y_pred'] == 1].index:
         sent_id, person, org, sent = idx
         f.write(f'{sent_id}\t{person}\tWork_For\t{org}\t{sent}\n')
 
-print(f'Relation extracted to: predicted_relation.txt')
+print(f'Relation extracted to: {output_file}')
