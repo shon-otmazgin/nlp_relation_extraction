2ba1932a2df27019f9e54930aa50bd39014d4c8a
shon-otmazgin <shon711@gmail.com>
2021-01-19 19:49:04 +0200

    train 0.741; 0.848

--- extract.py
+++ extract.py
@@ -2,6 +2,7 @@ import sys
 import pickle
 
 from features_extraction import build_df
+from rules import rule_retired, rule_org_s
 
 with open('trained_model', 'rb') as f:
     lr_clf, V, vocab = pickle.load(f)
@@ -15,6 +16,10 @@ output_file = sys.argv[2]
 with open(output_file, 'w', encoding="utf8") as f:
     for idx in test_df[test_df['y_pred'] == 1].index:
         sent_id, person, org, sent = idx
+        # if rule_retired(person=person, org=org, sent=sent):
+        #     continue
+        # if rule_org_s(person=person, org=org, sent=sent):
+        #     continue
         f.write(f'{sent_id}\t{person}\tWork_For\t{org}\t{sent}\n')
 
 print(f'Relation extracted to: {output_file}')
